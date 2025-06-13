0a656befaa7159657bc45949188d884c940a9b28
goodboy-kor <llaayy.kr@gmail.com>
2025-06-13 20:47:38 +0900

    msg test added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ package_name = 'bloom_for_you'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(include=[package_name]),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -13,13 +13,16 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='we',
-    maintainer_email='llaayy.kr@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='d-1',
+    maintainer_email='your@example.com',
+    description='Python-based ROS 2 package with custom messages.',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'flower_recommender = bloom_for_you.flower_recommender:main',
+            'pub_test = bloom_for_you.publisher_test:main',
+            'sub_test = bloom_for_you.subscriber_test:main',
         ],
     },
 )
