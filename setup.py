a1bf5b078a8bfea9563a67f000923621e2291cd1
goodboy-kor <llaayy.kr@gmail.com>
2025-06-16 13:49:47 +0900

    robot module done

--- setup.py
+++ setup.py
@@ -25,8 +25,6 @@ setup(
         'console_scripts': [
             'scenario_manager = bloom_for_you.scenario_manager:main',
             'flower_recommender = bloom_for_you.flower_recommender:main',
-            'pub_test = bloom_for_you.publisher_test:main',
-            'sub_test = bloom_for_you.subscriber_test:main',
             'test = bloom_for_you.test:main',
             'wrap = bloom_for_you.flower_wrapping:main',
             'water = bloom_for_you.watering_scheduler:main',            
