c3df47ca03a5baa05b817d5fe432827feb9ab43e
yeyeyeyeyeyeun <jyebubu@gmail.com>
2025-06-17 14:42:12 +0900

    wraping 초안

--- bloom_for_you/flower_wrapping.py
+++ bloom_for_you/flower_wrapping.py
@@ -14,22 +14,26 @@ from bloom_for_you.function_modules import robot
 
 ########### FlowerWrapping ############
 
-CMD_START_WRAPPING = 3
-CMD_PRINT_CARD = 4
+CMD_START_WRAPPING = 20
+CMD_PRINT_CARD = 21
+CMD_END_WRAPPING = 22
 
-POS_ID = []
-POS_BAG = []
-POS_TABLE = []
+POS_TABLE = [280.28, 63.48, 87.74, 20.24, -179.96, 19.97]
+POS_ZONE1 = [108.99, -461.86, 197.16, 104.45, -178.30, -168.57]
+POS_ZONE2 = [-114.80, -460.45, 197.16, 69.36, 180.00, 163.36]
+POS_PLANT = [POS_TABLE, POS_ZONE1, POS_ZONE2]
+
+POS_BAG = [423.94, -362.65, 291.63, 135.36, -171.82, 166.74]
 POS_CARD = []
+POS_PICKUP = []
 
 class FlowerWrapping(Node):
     def __init__(self):
         super().__init__("wrapping_node")
-        self.cmd_sub = self.create_subscription(FlowerInfo, 'flowerinfo', self.wrap_flower, 10)
-        self.card_cli = self.create_client(CardSrv,'cardsrv',10)
-
-        while not self.card_cli.wait_for_service(timeout_sec = 0.1):
-            self.get_logger().warning("카드 서버 준비 안됨")
+        self.cmd_sub = self.create_subscription(FlowerInfo, 'flower_info', self.wrap_flower, 10)
+        self.cmd_pub = self.create_publisher(FlowerInfo, 'flower_info',qos_profile=10)
+        self.card_cli = self.create_client(CardSrv,'cardsrv')
+        self.robot = robot.Robot()
             
 
     def wrap_flower(self, msg):
@@ -38,6 +42,7 @@ class FlowerWrapping(Node):
             return
         
         self.id = msg.id
+        self.zone_number = msg.zone_number
         self.flower_name = msg.flower_name
         self.flower_meaning = msg.flower_meaning
         self.growth_duration_days = msg.growth_duration_days
@@ -47,20 +52,61 @@ class FlowerWrapping(Node):
         tts("포장을 시작합니다.")
         self.get_logger().info("포장을 시작합니다.")
 
-        self._get_box()
+        self.robot.move_home()
+        self.robot.close_grip()
+        # self.robot.move(POS_PLANT[0])
+
+        self._get_bag()
+        # self._get_flower()
+        # self._get_card()
         
         tts('포장이 완료되었습니다.')
         self.get_logger().info("포장이 완료되었습니다.")
 
 
-    def _get_box(self):
-        self.get_logger().info("박스/쇼핑백 가져오는 중...")
+    def _get_bag(self):
+        self.get_logger().info("쇼핑백 가져오는 중...")
+
+        self.robot.open_grip()
+        self.robot.move_relative([0,0,-60,0,0,0])
+        self.robot.close_grip()
+        self.robot.move_relative([0,0,60,0,0,0])
+        self.robot.move(POS_TABLE)
+        self.robot.force_on_z(-10)
+        self.robot.check_touch(max=7)
+        self.robot.open_grip()
+        self.robot.move_home()
+
+        self.get_logger().info("쇼핑백 픽업 완료")
         
-        self._get_flower()
 
 
     def _get_flower(self):
         self.get_logger().info("화분 가져오는 중...")
+        
+        self.robot.move_home()
+
+        self.robot.move(POS_PLANT[self.zone_number])
+        self.robot.open_grip()
+        self.robot.move_relative([0,0,-20,0,0,0])
+        self.robot.close_grip()
+        self.robot.move_relative([0,0, 20,0,0,0])
+
+        self.robot.move(POS_PLANT[0])
+        self.robot.move_relative([0,0,-20,0,0,0])
+        self.robot.force_on_z(-10)
+        self.robot.check_touch(max=10)
+        self.robot.force_off()
+        self.robot.open_grip()
+        self.robot.move_relative([0,0,20,0,0,0])
+        self.robot.close_grip()
+        self.robot.move_home()
+        
+
+
+    def _get_card(self):
+        while not self.card_cli.wait_for_service(timeout_sec = 0.1):
+            self.get_logger().warning("카드 서버 준비 안됨")
 
         future = self.request_card_print()
         rclpy.spin_until_future_complete(self, future)
@@ -76,7 +122,46 @@ class FlowerWrapping(Node):
             self.get_logger().info("카드 출력이 완료되었습니다.")
             self._insert_card()
         
+        
+    def _insert_card(self):
+        self.get_logger().info("카드 넣는 중...")
+        self.robot.move_home()
+        self.robot.move(POS_CARD)
+        self.robot.open_grip()
+        self.robot.move_relative([20,0,0,0,0,0])
+        self.robot.close_grip()
+        self.robot.move_relative([-20,0,0,0,0,0])
+        self.robot.move(POS_PLANT[0])
+        self.robot.open_grip()
+        self.get_logger().info("카드 넣기 완료")
+        
+        self._offer_bag()
+        
 
+    def _offer_bag(self):
+        self.get_logger().info("사용자에게 꽃 전달 중...")
+        self.robot.move_home()
+
+        self.robot.move(POS_PLANT[0])
+        self.robot.open_grip()
+        self.robot.move_relative([0,0,-20,0,0,0])
+        self.robot.close_grip()
+        self.robot.move_relative([0,0,20,0,0,0])
+
+        self.robot.move(POS_PICKUP)
+        self.robot.force_on_z(-10)
+        self.robot.check_touch(max=10)
+        self.robot.force_off()
+        self.robot.open_grip()
+        self.robot.move(POS_PICKUP)
+        self.robot.close_grip()
+
+        self.robot.move_home()
+
+        self.complete_wrap()
+        self.get_logger().info("사용자에게 전달 완료")
+        
+        
     def request_card_print(self):
         self.get_logger().info("카드를 출력중입니다...")
         info = FlowerInfo(
@@ -88,22 +173,40 @@ class FlowerWrapping(Node):
             watering_cycle = self.watering_cycle,
             growth_state = self.growth_state
         )
+        
         request = CardSrv.Request()
         request.flowerinfo = info
         
         future = self.card_cli.call_async(request)
         return future
+    
+    def complete_wrap(self):
+        msg = FlowerInfo()
+        msg.id = self.id
+        msg.zone_number = self.zone_number
+        msg.command = CMD_END_WRAPPING
+        msg.flower_name = self.flower_name
+        msg.flower_meaning = self.flower_meaning
+        msg.growth_duration_days = self.growth_duration_days
+        msg.watering_cycle = self.watering_cycle
+
+        self.cmd_pub.publish(msg)
+        self.get_logger().info("꽃 정보 전달 완료")
+
+        return
+
         
-        
-    def _insert_card(self):
-        self.get_logger().info("카드 넣는 중...")
+
+
+    
+
         
 
 def main(args=None):
-    rclpy.init(args=args)
+    # rclpy.init(args=args)
     node = FlowerWrapping()
     try:
-        rclpy.spin_once(node)
+        rclpy.spin(node)
     finally:
         node.destroy_node()
         rclpy.shutdown()
--- bloom_for_you/watering_scheduler.py
+++ bloom_for_you/watering_scheduler.py
@@ -3,47 +3,18 @@ import time
 import datetime
 import cv2
 import sys
-from scipy.spatial.transform import Rotation
 import numpy as np
 import rclpy
 from rclpy.node import Node
-import DR_init
 
 from bloom_for_you.function_modules.tts import tts
-from bloom_for_you.function_modules.onrobot import RG
+
+from bloom_for_you.function_modules import robot
 
 from bloom_for_you.function_modules.realsense import ImgNode
 from bloom_for_you.function_modules.yolo import YoloModel
 
 from bloom_for_you_interfaces.msg import FlowerInfo
-
-# for single robot
-# ROBOT_ID = "dsr01"
-# ROBOT_MODEL = "m0609"
-# VELOCITY, ACC = 40, 40
-# GRIPPER_NAME = "rg2"
-# TOOLCHARGER_IP = "192.168.1.1"
-# TOOLCHARGER_PORT = "502"
-# DEPTH_OFFSET = -15.0
-# MIN_DEPTH = 2.0
-
-# DR_init.__dsr__id = ROBOT_ID
-# DR_init.__dsr__model = ROBOT_MODEL
-
-# rclpy.init()
-# dsr_node = rclpy.create_node("robot_control_node", namespace=ROBOT_ID)
-# DR_init.__dsr__node = dsr_node
-
-# try:
-#     from DSR_ROBOT2 import movej, movel, mwait, DR_MV_MOD_REL
-# except ImportError as e:
-#     print(f"Error importing DSR_ROBOT2: {e}")
-#     sys.exit()
-
-########### Gripper Setup. Do not modify this area ############
-
-# gripper = RG(GRIPPER_NAME, TOOLCHARGER_IP, TOOLCHARGER_PORT)
-
 
 ########### FlowerWatering ############
 
@@ -51,8 +22,8 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 # package_path = "/home/rokey/r2_ws/src/bloom_for_you"
 package_path = os.path.abspath(os.path.join(current_dir, ".."))
 
-CMD_START_WATERING = 5
-CMD_CHECK_GROWTH_COMPLETE = 6
+CMD_START_WATERING = 30
+CMD_END_WATERING = 31
 
 POS_ID = []
 POS_WATER = []
@@ -63,8 +34,9 @@ POS_SUPPORT_ZONE = []
 class FlowerWatering(Node):
     def __init__(self):
         super().__init__("watering_node")
-        self.cmd_sub = self.create_subscription(FlowerInfo, 'flowerinfo', self.water_the_flower, 10)
-        self.growth_pub = self.create_publisher(FlowerInfo, 'flowerinfo', 10)
+        self.cmd_sub = self.create_subscription(FlowerInfo, 'flower_info', self.water_the_flower, 10)
+        self.growth_pub = self.create_publisher(FlowerInfo, 'flower_info', 10)
+        self.img_node = ImgNode()
 
 
     def water_the_flower(self, msg):
@@ -97,27 +69,10 @@ class FlowerWatering(Node):
         self._check_growth()
 
     def _check_growth(self):
-        msg = FlowerInfo()
-        msg.id = self.id
-        msg.command = CMD_CHECK_GROWTH_COMPLETE
-        msg.flower_name = self.flower_name
-        msg.flower_meaning = self.flower_meaning
-        msg.growth_duration_days = self.growth_duration_days
-        msg.watering_cycle = self.watering_cycle
-    
-        
-        
-        grow_state = 1
-        self.growth_state=grow_state
-        
-        
-        
-        
-        msg.growth_state = self.growth_state
-        self.growth_pub.publish(msg)
         self._take_pictures()
 
     def _take_pictures(self):
+        rclpy.spin_once(self.img_node)
         color_image = self.img_node.get_color_frame()
         if color_image is not None:
             timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -135,6 +90,21 @@ class FlowerWatering(Node):
         self._return_flower()
 
     def _return_flower(self):
+        msg = FlowerInfo()
+        msg.id = self.id
+        msg.command = CMD_END_WATERING
+        msg.flower_name = self.flower_name
+        msg.flower_meaning = self.flower_meaning
+        msg.growth_duration_days = self.growth_duration_days
+        msg.watering_cycle = self.watering_cycle
+    
+        
+        grow_state = 1
+        self.growth_state=grow_state
+        
+        msg.growth_state = self.growth_state
+        self.growth_pub.publish(msg)
+
         tts("물 주기를 완료하였습니다.")
         self.get_logger().info("물 주기 완료")
 
