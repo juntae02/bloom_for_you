ab6b92a1ba9094ca447fc823ffb2b0deffc3cd14
yeyeyeyeyeyeun <jyebubu@gmail.com>
2025-06-17 15:52:30 +0900

    wrap mod

--- bloom_for_you/flower_wrapping.py
+++ bloom_for_you/flower_wrapping.py
@@ -18,7 +18,7 @@ CMD_START_WRAPPING = 20
 CMD_PRINT_CARD = 21
 CMD_END_WRAPPING = 22
 
-POS_TABLE = [280.28, 63.48, 87.74, 20.24, -179.96, 19.97]
+POS_TABLE = [280.28, 63.48, 187.74, 20.24, -179.96, 19.97]
 POS_ZONE1 = [108.99, -461.86, 197.16, 104.45, -178.30, -168.57]
 POS_ZONE2 = [-114.80, -460.45, 197.16, 69.36, 180.00, 163.36]
 POS_PLANT = [POS_TABLE, POS_ZONE1, POS_ZONE2]
@@ -54,11 +54,10 @@ class FlowerWrapping(Node):
 
         self.robot.move_home()
         self.robot.close_grip()
-        # self.robot.move(POS_PLANT[0])
 
         self._get_bag()
-        # self._get_flower()
-        # self._get_card()
+        self._get_flower()
+        self._get_card()
         
         tts('포장이 완료되었습니다.')
         self.get_logger().info("포장이 완료되었습니다.")
@@ -67,15 +66,24 @@ class FlowerWrapping(Node):
     def _get_bag(self):
         self.get_logger().info("쇼핑백 가져오는 중...")
 
+        self.robot.move(POS_BAG)
+        self.get_logger().info("1")
         self.robot.open_grip()
         self.robot.move_relative([0,0,-60,0,0,0])
         self.robot.close_grip()
+        self.get_logger().info("2")
         self.robot.move_relative([0,0,60,0,0,0])
         self.robot.move(POS_TABLE)
+        self.get_logger().info("3")
         self.robot.force_on_z(-10)
         self.robot.check_touch(max=7)
+        self.robot.force_off()
+        self.get_logger().info("4")
+
         self.robot.open_grip()
+        self.get_logger().info("5")
         self.robot.move_home()
+        self.get_logger().info("6")
 
         self.get_logger().info("쇼핑백 픽업 완료")
         
@@ -97,6 +105,7 @@ class FlowerWrapping(Node):
         self.robot.force_on_z(-10)
         self.robot.check_touch(max=10)
         self.robot.force_off()
+
         self.robot.open_grip()
         self.robot.move_relative([0,0,20,0,0,0])
         self.robot.close_grip()
@@ -109,24 +118,15 @@ class FlowerWrapping(Node):
             self.get_logger().warning("카드 서버 준비 안됨")
 
         future = self.request_card_print()
-        rclpy.spin_until_future_complete(self, future)
-
-        try:
-            response = future.result()
-        except Exception as e:
-            self.get_logger().warn(f"Service call failed: {str(e)}")
-            return
-        
-        self.get_logger().info(f"Result: {response.success}")
-        if response.success:
-            self.get_logger().info("카드 출력이 완료되었습니다.")
-            self._insert_card()
+        self.get_logger().info("11111111111111111111")
+        future.add_done_callback(self._card_done_callback)
+        self.get_logger().info("22222222222222222222222222")
         
         
     def _insert_card(self):
         self.get_logger().info("카드 넣는 중...")
         self.robot.move_home()
-        self.robot.move(POS_CARD)
+        # self.robot.move(POS_CARD)
         self.robot.open_grip()
         self.robot.move_relative([20,0,0,0,0,0])
         self.robot.close_grip()
@@ -148,12 +148,12 @@ class FlowerWrapping(Node):
         self.robot.close_grip()
         self.robot.move_relative([0,0,20,0,0,0])
 
-        self.robot.move(POS_PICKUP)
+        # self.robot.move(POS_PICKUP)
         self.robot.force_on_z(-10)
         self.robot.check_touch(max=10)
         self.robot.force_off()
         self.robot.open_grip()
-        self.robot.move(POS_PICKUP)
+        # self.robot.move(POS_PICKUP)
         self.robot.close_grip()
 
         self.robot.move_home()
@@ -180,6 +180,18 @@ class FlowerWrapping(Node):
         future = self.card_cli.call_async(request)
         return future
     
+    def _card_done_callback(self, future):
+        try:
+            response = future.result()
+        except Exception as e:
+            self.get_logger().warn(f"Service call failed: {str(e)}")
+            return
+
+        self.get_logger().info(f"카드 응답 받음: {response.success}")
+        if response.success:
+            self.get_logger().info("카드 출력 완료됨")
+            self._insert_card()
+    
     def complete_wrap(self):
         msg = FlowerInfo()
         msg.id = self.id
@@ -195,7 +207,7 @@ class FlowerWrapping(Node):
 
         return
 
-        
+    
 
 
     
